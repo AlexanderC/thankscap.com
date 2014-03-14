ef77c8afc75387435daaf16ce16773133d6127e5
AlexanderC <alexander.cucer@flosites.com>
2014-03-14 20:29:02 +0200

    added js costumization

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
 $().ready(function(){
-    $(".main").onepage_scroll({
+    var cont = $(".main");
+
+    cont.onepage_scroll({
         sectionContainer: "section",
         responsiveFallback: 600,
         loop: true
     });
 
     var author = $(".author-details > a");
+    var hash = window.location.hash || false;
+    var plainUrl = hash ? document.URL.replace(/#.*$/, "") : document.URL;
 
-    var authorName = getParameterByName("n");
-    var authorEmail = getParameterByName("e");
+    if(hash) {
+        hash = hash.replace("#", "");
 
-    if(authorEmail && authorName) {
-        author.attr("href", "mailto:" + authorEmail + "?subject=Hi%20" + encodeURIComponent(authorName) + ",%20Captain%20Obvious%20is%20mailing%20you!");
-        author.text(authorName);
+        try {
+            var data = JSON.parse($.base64.decode(hash));
+        } catch(e){
+            var data = false;
+        }
+
+        if(data && data.name && data.email) {
+            author.attr("href", "mailto:" + data.email + "?subject=Hi%20" + encodeURIComponent(data.name) + ",%20Captain%20Obvious%20is%20mailing%20you!");
+            author.text(data.name);
+        }
     }
+
+    // bind form submit
+    $("form.personalize-form").unbind("submit");
+    $("form.personalize-form").submit(function(e) {
+        e.preventDefault();
+
+        var form = $(this);
+
+        var data = {
+            name: form.find("input[name='name']").val(),
+            email: form.find("input[name='email']").val()
+        };
+
+        var newHash = $.base64.encode(JSON.stringify(data));
+        var newUrl = plainUrl + "#" + newHash;
+
+        window.history.replaceState('Object', 'Title', newUrl);
+
+        author.attr("href", "mailto:" + data.email + "?subject=Hi%20" + encodeURIComponent(data.name) + ",%20Captain%20Obvious%20is%20mailing%20you!");
+        author.text(data.name);
+
+        // move to first page
+        cont.moveTo(1);
+
+        // show arrow that indicated to the status bar
+        var arrow = $("div.arrow");
+        arrow.fadeIn("slow", function() {
+            $(this).delay(5400).fadeOut("fast");
+        });
+
+        return false;
+    });
 });
 
 /* some additional functionality */
-
-function getParameterByName(name) {
-    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
-}
